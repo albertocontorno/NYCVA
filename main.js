6b0859d4b0e410d3b96d9b873930a09e64b0d04d
Alberto Contorno <alberto.contorno@gmail.com>
2020-02-07 13:30:28 +0100

    d3 fun

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log(d3)
+setInterval( () =>d3.selectAll("p").style("color", function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+}), 500);
+setInterval( () =>d3.select("body").transition().style("background-color", function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+}), 500);
+setInterval( () => d3.selectAll("p")
+  .data([Math.round(Math.random()*50), Math.round(Math.random()*50), Math.round(Math.random()*50), Math.round(Math.random()*50), Math.round(Math.random()*50), Math.round(Math.random()*50)])
+  .transition().style("font-size", function(d) { return d + "px"; }), 500);
+
